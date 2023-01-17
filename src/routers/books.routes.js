68f96f04aa66a6aa4e454f48c6a953228477ae89
Robin9203 <robinxd11@gmail.com>
2023-01-17 12:38:34 +0100

    Segundo commit

--- src/routers/books.routes.js
+++ src/routers/books.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { getbooks } = require('../controllers/books.controllers')
-
+console.log('hola')
 const routers = Router()
 
 routers.get('/', getbooks)

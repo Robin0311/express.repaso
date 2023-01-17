76a8a5b04541630c9b2524ec0b124d3356f12b85
Robin9203 <robinxd11@gmail.com>
2023-01-17 13:18:48 +0100

    feat: Update tres

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ console.log("Hola " + process.env.PORT);
 const { application } = require("express");
 const express = require("express");
 const app = express();
+const { dbConnection } = require("./config/db");
 
 app.listen(process.env.PORT, () => {
-  console.log(`\x1b[32m ******************************************************* \x1b[0m`);
-  console.log(`\x1b[32m **  Se levanta mi API en el puerto ${process.env.PORT}  ** \x1b[0m`);
-  console.log(`\x1b[32m ******************************************************* \x1b[0m`);
+  console.log(
+    `\x1b[32m ******************************************************* \x1b[0m`
+  );
+  console.log(
+    `\x1b[32m **  Se levanta mi API en el puerto ${process.env.PORT}  ** \x1b[0m`
+  );
+  console.log(
+    `\x1b[32m ******************************************************* \x1b[0m`
+  );
 });
+
+dbConnection();
 
 // localhost:5000
 // localhost:5000/
-app.get('/', (req, res) => {
-    console.log('Estoy en home')
-    res.send('Hola estan en mi API')
-})
+app.get("/", (req, res) => {
+  console.log("Estoy en home");
+  res.send("Hola estan en mi API");
+});
 
 // localhost:5000/books
-app.use('/books', require('./routers/books.routes'))
+app.use("/books", require("./routers/books.routes"));
